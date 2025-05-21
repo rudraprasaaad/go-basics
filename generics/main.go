8ef6b1f6caa6eac6a8edae140779c678c4402f62
rudraprasaaad <goudarudra23@gmail.com>
2025-05-21 12:10:17 +0530

    some generics code

---
+++ generics/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Number interface {
+	int64 | float64
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non generic sums: %v\n", SumNumbers(ints))
+	fmt.Printf("Non generic sums: %v\n", SumNumbers(floats))
+}
